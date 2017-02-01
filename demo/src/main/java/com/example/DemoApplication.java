b46e6c0ee0a7e8a2e66d54d8e06f01266b4c640a
Angel Pimentel <angelpmza@gmail.com>
2017-01-31 18:44:04 -0600

    Added repeat

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
-		NotificationService notificationService = applicationContext.getBean(NotificationService.class);
-		notificationService.notifySystem();
-	}
-
+    public static void main(String[] args) {
+        Long number = Long.parseLong(args[0]);
+        ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+        NotificationService notificationService = applicationContext.getBean(NotificationService.class);
+        for (int i = 0; i < number; i++) {
+            notificationService.notifySystem();
+        }
+    }
 }
